2500bbed215cff0eff11ea531f480d8ae2350105
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-06-12 15:15:24 +0000

    Mumuki Export on 2019-06-12 15:15:24 +0000

---
+++ 00010_Não esqueça de colocar o where___/extra.sql
@@ -0,0 +1,13 @@
+CREATE TABLE bandas(
+  id INTEGER PRIMARY KEY,
+  nome TEXT NOT NULL,
+  premios INTEGER
+);
+
+INSERT into bandas values (null,"Iron Maiden", 0);
+INSERT into bandas values (null,"The Strokes", 42);
+INSERT into bandas values (null,"The Beatles", 58);
+INSERT into bandas values (null,"AC DC", 35);
+INSERT into bandas values (null,"Metallica", 25);
+INSERT into bandas values (null,"Pink Floyd", 36);
+INSERT into bandas values (null,"Nirvana", 12);
