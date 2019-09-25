b790d22059a37e2730fc4abaaa2c7dd615aa37f8
vtorres@digitalhouse.com <vtorres@digitalhouse.com>
2019-09-25 19:59:22 +0000

    Mumuki Export on 2019-09-25 19:59:22 +0000

--- 00013_Sem apelido/extra.sql
+++ 00013_Sem apelido/extra.sql
@@ -6,7 +6,7 @@ CREATE TABLE artistas(
   apelido TEXT
 );
 
-INSERT into artistas values(null, "Carlos", "Gonzaga", "1950-01-23", null);
+INSERT into artistas values(null, "Carlos", "Gonzaga", "1950-01-23", 'Gonzaguinha');
 INSERT into artistas values(null, "Caetano", "Veloso", "1951-10-23",null);
 INSERT into artistas values(null, "Felipe", "Araújo", "1959-07-23",null);
-INSERT into artistas values(null, "Jair", "Rodrigues", "1963-03-13",null);
+INSERT into artistas values(null, "Jair", "Rodrigues", "1963-03-13",'Jairzinho');
