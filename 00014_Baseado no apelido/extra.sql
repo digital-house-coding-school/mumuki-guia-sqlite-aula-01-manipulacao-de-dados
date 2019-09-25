636d2451ccb17f25cca2d9517cf7bb9ed3b1caef
vtorres@digitalhouse.com <vtorres@digitalhouse.com>
2019-09-25 20:12:45 +0000

    Mumuki Export on 2019-09-25 20:12:45 +0000

--- 00014_Baseado no apelido/extra.sql
+++ 00014_Baseado no apelido/extra.sql
@@ -3,11 +3,11 @@ CREATE TABLE artistas(
   nome TEXT NOT NULL,
   sobrenome TEXT NOT NULL,
   data_de_nascimento TEXT,
-  apodo TEXT
+  apelido TEXT
 );
 
-INSERT into artistas values(null, "Carlos", "Gonzaga", "1951-10-23", null);
-INSERT into artistas values(null, "Caetano", "Veloso", "1950-01-23",null);
-INSERT into artistas values(null, "Felipe", "Araújo", "1959-07-23",null);
-INSERT into artistas values(null, "Jair", "Rodrigues", "1963-03-13",null);
+INSERT into artistas values(null, "Carlos", "Gonzaga", "1951-10-23", 'Gonzaguinha');
+INSERT into artistas values(null, "Caetano", "Veloso", "1950-01-23",'Sem apelido');
+INSERT into artistas values(null, "Felipe", "Araújo", "1959-07-23",'Sem apelido');
+INSERT into artistas values(null, "Jair", "Rodrigues", "1963-03-13",'Jairzinho');
 INSERT into artistas values(null, "", "", "","Luizinho");
