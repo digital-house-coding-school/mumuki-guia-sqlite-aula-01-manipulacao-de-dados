1fb23dbb8952487e57ac09d3f02cff1d5dc6c0eb
vtorres@digitalhouse.com <vtorres@digitalhouse.com>
2019-09-26 17:59:38 +0000

    Mumuki Export on 2019-09-26 17:59:38 +0000

--- 00008_Relações___/extra.sql
+++ 00008_Relações___/extra.sql
@@ -8,6 +8,14 @@ INSERT into bandas values (null,"Iron Maiden", null);
 INSERT into bandas values (null,"The Strokes", 42);
 INSERT into bandas values (null,"The Beatles", 58);
 
+CREATE TABLE artistas(
+  id INTEGER PRIMARY KEY,
+  nome TEXT NOT NULL,
+  sobrenome TEXT NOT NULL,
+  data_de_nascimento TEXT
+);
+
+
 CREATE TABLE banda_artista(
   id INTEGER PRIMARY KEY,
   id_banda INTEGER,
